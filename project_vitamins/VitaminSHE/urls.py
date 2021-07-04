d623a58b085143a41683f5eb6c5c5ca5a4fc6b0f
smrithi1912 <53621812+smrithi1912@users.noreply.github.com>
2021-07-04 11:36:51 +0530

    adding all pages

--- project_vitamins/VitaminSHE/urls.py
+++ project_vitamins/VitaminSHE/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.home, name='VitaminSHE-home'),
-    path('about/', views.about, name='VitaminSHE-about'),
+    path('signup/', views.signup, name='VitaminSHE-signup'),
+    path('login/', views.login, name='VitaminSHE-login'),
+    path('healthcheck/', views.healthcheck, name='VitaminSHE-healthcheck'),
+    path('food/', views.login, name='VitaminSHE-food'),
+    path('book/', views.login, name='VitaminSHE-book'),
+    path('why/', views.login, name='VitaminSHE-why'),
 ]
-
--- project_vitamins/VitaminSHE/views.py
+++ project_vitamins/VitaminSHE/views.py
@@ -4,6 +4,21 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'VitaminSHE/home.html')
 
-def about(request):
-    return HttpResponse('<h1>Webapp About Page</h1>')
+def signup(request):
+    return render(request, 'VitaminSHE/signup.html')
+
+def login(request):
+    return render(request, 'VitaminSHE/login.html')
+
+def healthcheck(request):
+    return render(request, 'VitaminSHE/healthcheck.html')
+
+def food(request):
+    return render(request, 'VitaminSHE/food.html')
+
+def book(request):
+    return render(request, 'VitaminSHE/book.html')
+
+def why(request):
+    return render(request, 'VitaminSHE/why.html')
 
