1dd34fde42b8d736ca5ee3141ed9ced437095447
Eesha Krishnamagaru <ekrishnamagaru@Eeshas-MacBook-Pro.local>
2021-08-03 12:57:30 -0700

    VitaminSHE.py updated & factors in pregnancy + lactation

--- VitaminSHE.py
+++ VitaminSHE.py
@@ -1,41 +1,25 @@
 import nltk; nltk.download('popular')
 
-
-# Essential Vitamins (all in milligrams): {"Age Range": (darily required intake)}
-vitamin_a = {"1-3": 0.600, "4-8": 0.900, "9-13": 1.7, "14-18": 2.8, "19-30": 3.0, "31-50": 3.0, "51-70": 3.0, "71+": 3.0}
-vitamin_e = {"1-3": 200, "4-8": 300, "9-13": 600, "14-18": 800, "19-30": 1000, "31-50": 1000, "51-70": 1000, "71+": 1000}
-vitamin_c = {"1-3": 400, "4-8": 650, "9-13": 1200, "14-18": 1800, "19-30": 2000, "31-50": 2000, "51-70": 2000, "71+": 2000}
-vitamin_d = {"1-3": 0.063, "4-8": 0.090, "9-13": 0.017, "14-18": 0.028, "19-30": 3.0, "31-50": 3.0, "51-70": 3.0, "71+": 3.0}
-
-vitamin_b_6 = {"1-3": 0.030, "4-8": 0.040, "9-13": 0.060, "14-18": 0.080, "19-30": 0.100, "31-50": 0.100, "51-70": 0.100, "71+": 0.100}
-vitaminb_b_12 = {"1-3": 0.0009, "4-8": 0.0012, "9-13": 0.0018, "14-18": 0.0024, "19-30": 0.0024, "31-50": 0.0024, "51-70": 0.0024, "71+": 0.0024}
-# Does not factor in pregnancy and breastfeeding at the moment
-
-
-# Essential Minerals (all in milligrams): {"Age Range": (daily required intake)
-calcium = {"1-3": 0.700, "4-8": 1.000, "9-13": 1.300, "14-18": 1.300, "19-30": 1.000, "31-50": 1.000, "51-70": 1.200, "71+": 1.200}
-iron = {"1-3": 7, "4-8": 10, "9-13": 8, "14-18": 15, "19-30": 18, "31-50": 18, "51-70": 8, "71+": 8}
-magnesium = {"1-3": 80, "4-8": 130, "9-13": 240, "14-18": 360, "19-30": 310, "31-50": 320, "51-70": 320, "71+": 320}
-potassium = {"1-3": 2000, "4-8": 2300, "9-13": 2300, "14-18": 2300, "19-30": 2600, "31-50": 2600, "51-70": 2600, "71+": 2600}
-zinc = {"1-3": 3, "4-8": 5, "9-13": 8, "14-18": 9, "19-30": 8, "31-50": 8, "51-70": 8, "71+": 8}
-# Does not factor in pregnancy and breastfeeding at the moment
-
-#Symptoms of Vitamin and Mineral Deficiency 
-vitamin_a_symptoms = """dry eyes, dry skin, frequent infections, spots in the eyeball, inability to see in dim light"""
 """
-vitamin_a_deficiency  = poor night vision, dry eyes dry skin, frequent infections, 'spots in the eyes'] 
-vitamin_b_6_deficiency = ['depression', 'numbness', 'anemia']
-vitamin_b_12_deficiency = ['depression', 'poor concentration', 'numbness']
-vitamin_c_deficiency = ['depression', 'bruising', 'fatigue', 'bleeding gums', 'tooth loss']
-vitamin_d_deficiency = ['twitching', 'muscle pain and cramps', 'irregular menstruation', 'weak bones', 'hair loss', 'high blood pressure']
-vitamin_e_deficiency = ['unsteady movement or walking']
-
-calcium_deficiency = ['twitching', 'muscle cramping', 'twitching', 'spasms']
-iron_deficiency = ['poor concentration', 'irregular menstruation', 'fatigue', 'anemia']
-magnesium_deficiency = ['palpitations', 'constipation', 'twitching', 'muscle pain and cramps']
-potassium_deficiency = ['palpitations', 'constipation', 'muscle pain and cramps']
-zinc_deficiency = ['poor night vision', 'loss of appetite', 'loss of taste', 'hair loss', 'diarrhea']
+Source: Otten, Jennifer J, et al. “Dietary Reference Intakes: The Essential Guide to Nutrient Requirements.” National Academies Press: OpenBook, 2006, www.nap.edu/read/11537/chapter/1#xii.  
 """
+
+# Essential Vitamins for Females (all in milligrams): {"Age Range": (daily required intake)}
+f_vitamin_a = {"Infant": 0.500,"1-3": 0.300, "4-8": 0.400, "9-13": 0.600, "14-18": 0.700, "19-30": 0.700, "31-50": 0.700, "51-70": 0.700, "71+": 0.700, "Preg. <= 18": 0.750, "Preg. 19+": 0.770, "Lact. <= 18": 1.2, "Lact. 19+": 1.3}
+f_vitamin_b_6 = {"Infant": 0.300, "1-3": 0.500, "4-8": 0.600, "9-13": 1.000, "14-18": 1.300, "19-30": 1.300, "31-50": 1.300, "51-70": 1.700, "71+": 1.700, "Preg. <= 18": 1.900, "Preg. 19+": 1.900, "Lact. <= 18": 2.000, "Lact. 19+": 2.000}
+f_vitaminb_b_12 = {"Infant": 0.0005, "1-3": 0.0009, "4-8": 0.0012, "9-13": 0.0018, "14-18": 0.0024, "19-30": 0.0024, "31-50": 0.0024, "51-70": 0.0024, "71+": 0.0024, "Preg. <= 18": 0.0026, "Preg. 19+": 0.0026, "Lact. <= 18": 0.0028, "Lact. 19+": 0.0028}
+f_vitamin_c = {"Infant": 45,"1-3": 15, "4-8": 25, "9-13": 45, "14-18": 65, "19-30": 75, "31-50": 75, "51-70": 75, "71+": 75, "Preg. <= 18": 80, "Preg. 19+": 85, "Lact. <= 18": 115, "Lact. 19+": 120}
+f_vitamin_d = {"Infant": 0.015,"1-3": 0.0275, "4-8": 0.0275, "9-13": 0.0275, "14-18": 0.0275, "19-30": 0.0275, "31-50": 0.0275, "51-70": 0.030, "71+": 0.0325, "Preg. <= 18": 0.0275, "Preg. 19+": 0.0275, "Lact. <= 18": 0.0275, "Lact. 19+": 0.0275}
+f_vitamin_e = {"Infant": 5.000,"1-3":  6.000, "4-8":  7.000, "9-13": 11.000, "14-18": 15.000, "19-30": 15.000, "31-50": 15.000, "51-70": 15.000, "71+": 15.000, "Preg. <= 18": 15.000, "Preg. 19+": 15.000, "Lact. <= 18": 19.000, "Lact. 19+": 19.000}
+
+
+# Essential Minerals for Females (all in milligrams): {"Age Range": (daily required intake)
+f_calcium = {"Infant": 240,"1-3": 500, "4-8": 800, "9-13": 1300, "14-18": 1300, "19-30": 1000, "31-50": 1000, "51-70": 1200, "71+": 1200, "Preg. <= 18": 1300, "Preg. 19+": 1000, "Lact. <= 18": 1300, "Lact. 19+": 1000}
+f_iron = {"Infant": 11.000, "1-3": 7.000, "4-8": 10.000, "9-13": 8.000, "14-18": 15.000, "19-30": 18.000, "31-50": 18.000, "51-70": 8.000, "71+": 8.000, "Preg. <= 18": 27.000, "Preg. 19+": 27.000, "Lact. <= 18": 10.000, "Lact. 19+": 9.000}
+f_magnesium = {"Infant": 75.000,"1-3": 80.000, "4-8": 130.000, "9-13": 240.000, "14-18": 360.000, "19-30": 310.000, "31-50": 320.000, "51-70": 320.000, "71+": 320.000, "Preg. <= 18": 400.000, "Preg. 19+": 350.000, "Lact. <= 18": 360.000, "Lact. 19+": 310.000}
+f_potassium = {"Infant": 550.000,"1-3": 3000.000, "4-8": 3800.000, "9-13": 4500.000, "14-18": 4700.000, "19-30": 4700.000, "31-50": 4700.000, "51-70": 4700.000, "71+": 4700.000, "Preg. <= 18": 4700.000, "Preg. 19+": 4700.000, "Lact. <= 18": 5100.000, "Lact. 19+": 5100.000}
+f_zinc = {"Infant": 3.000,"1-3": 3.000, "4-8": 5.000, "9-13": 8.000, "14-18": 9.000, "19-30": 8.000, "31-50": 8.000, "51-70": 8.000, "71+": 8.000, "Preg. <= 18": 12.000, "Preg. 19+": 11.000, "Lact. <= 18": 13.000, "Lact. 19+": 12.000}
+
 # Creates a user 
 class User:
     def __init__(self, name=str, age=int, height=int, weight=int, pregnancy_status=str, breastfeeding_status=str, user_symptoms=str):
@@ -45,122 +29,84 @@ class User:
         self.weight = weight
         self.pregnancy_status = pregnancy_status
         self.breastfeeding_status = breastfeeding_status
-        self.user_symptoms = user_symptoms
         self.age_range = self.compute_age_range()
         
-        self.user_vitamin_a = vitamin_a[self.age_range]
-        self.user_vitamin_b_6 = vitamin_b_6[self.age_range]
-        self.user_vitamin_b_12 = vitaminb_b_12[self.age_range]
-        self.user_vitamin_c = vitamin_c[self.age_range]
-        self.user_vitamin_d = vitamin_d[self.age_range]
-        self.user_vitamin_e = vitamin_e[self.age_range]
+        self.user_vitamin_a = f_vitamin_a[self.age_range]
+        self.user_vitamin_b_6 = f_vitamin_b_6[self.age_range]
+        self.user_vitamin_b_12 = f_vitaminb_b_12[self.age_range]
+        self.user_vitamin_c = f_vitamin_c[self.age_range]
+        self.user_vitamin_d = f_vitamin_d[self.age_range]
+        self.user_vitamin_e = f_vitamin_e[self.age_range]
 
         
-        self.user_calcium = calcium[self.age_range]
-        self.user_iron = iron[self.age_range]
-        self.user_magnesium = magnesium[self.age_range]
-        self.uesr_potassium = potassium[self.age_range]
-        self.user_zinc = zinc[self.age_range]
+        self.user_calcium = f_calcium[self.age_range]
+        self.user_iron = f_iron[self.age_range]
+        self.user_magnesium = f_magnesium[self.age_range]
+        self.uesr_potassium = f_potassium[self.age_range]
+        self.user_zinc = f_zinc[self.age_range]
         
     
     def compute_age_range(self):
         """Figures out an age range for the user"""
+      
+        # Factors in Pregnancy and Lactation
+        if self.pregnancy_status == "Y" or self.breastfeeding_status == "Y":
+            
+            # If both pregnant and lactating, simulation assumes lactation label
+            if self.pregnancy_status == "Y" and self.breastfeeding_status == "Y":
+                if self.age <= 18: 
+                    self.age_range = "Lact. <= 18"
+                else:
+                    self.age_range = "Lact. 19+" 
+            
+            # Checks age and pregnancy status
+            if self.pregnancy_status == "Y":
+                if self.age <= 18:
+                    self.age_range = "Preg. <= 18"
+                else: 
+                    self.age_range = "Preg. 19+"
+            
+            # Checks age and lactation status
+            if self.breastfeeding_status == "Y":
+                if self.age <= 18:
+                    self.age_range = "Lact. <= 18"
+                else: 
+                    self.age_range = "Lact. 19+"
+            
+        else:
+            # Checks if user is in the age range of 1 to 3
+            if self.age < 0 & self.age >= 3:
+                self.age_range = "1-3"
+        
+            # Checks if user is in the age range of 4 to 8
+            if self.age >= 4 & self.age <= 8:
+                self.age_range = "4-8"
+        
+            # Checks if user is in the age range of 9 to 13
+            if self.age >= 9 & self.age <= 13:
+                self.age_range = "9-13"
+        
+            # Checks if user is in the age range of 14 to 18
+            if self.age >= 14 & self.age <= 18:
+                self.age_range = "14-18"
 
-        # Checks if user is in the age range of 1 to 3
-        if self.age < 0 & self.age >= 3:
-            self.age_range = "1-3"
+            # Checks if user is in the age range of 19 to 30
+            if self.age >= 19 & self.age <= 30:
+                self.age_range = "19-30"
         
-        # Checks if user is in the age range of 4 to 8
-        if self.age >= 4 & self.age <= 8:
-            self.age_range = "4-8"
+            # Checks if user is in the age range of 31 to 50
+            if self.age >= 31 & self.age <= 50:
+                self.age_range = "31-50"
         
-        # Checks if user is in the age range of 9 to 13
-        if self.age >= 9 & self.age <= 13:
-            self.age_range = "9-13"
+            # Checks if user is in the age range of 51 to 70
+            if self.age >= 51 & self.age <= 70:
+                self.age_range = "51-70"
         
-        # Checks if user is in the age range of 14 to 18
-        if self.age >= 14 & self.age <= 18:
-            self.age_range = "14-18"
-
-        # Checks if user is in the age range of 19 to 30
-        if self.age >= 19 & self.age <= 30:
-            self.age_range = "19-30"
-        
-        # Checks if user is in the age range of 31 to 50
-        if self.age >= 31 & self.age <= 50:
-            self.age_range = "31-50"
-        
-        # Checks if user is in the age range of 51 to 70
-        if self.age >= 51 & self.age <= 70:
-            self.age_range = "51-70"
-        
-        # Checks if user is in the age range of 71+
-        if self.age >= 71:
-            self.age_range = "71+"
+            # Checks if user is in the age range of 71+
+            if self.age >= 71:
+                self.age_range = "71+"
         
         return self.age_range
-
-    
-    def analyze_symptoms(self):
-        keywords = nltk.word_tokenize(self.user_symptoms)
-        v_a_counter = 0
-        v_b_6_counter = 0
-        v_b_12_counter = 0
-        v_c_counter = 0
-        v_d_counter = 0
-        v_e_counter = 0
-
-        calcium_counter = 0
-        iron_counter = 0
-        magnesium_counter = 0
-        potassium_counter = 0
-        zinc_counter = 0
-
-
-        for k in keywords:
-
-            if k in vitamin_a_deficiency:
-                v_a_counter += 1
-
-            if k in vitamin_b_6_deficiency:
-                v_b_6_counter += 1
-            
-            if k in vitamin_b_12_deficiency:
-                v_b_12_counter += 1
-
-            if k in vitamin_c_deficiency:
-                v_c_counter += 1
-
-            if k in vitamin_d_deficiency:
-                v_d_counter += 1
-
-            if k in vitamin_e_deficiency:
-                v_e_counter += 1
-            
-            if k in calcium_deficiency:
-                calcium_counter += 1
-            
-            if k in iron_deficiency:
-                iron_counter += 1
-            
-            if k in magnesium_deficiency:
-                magnesium_counter += 1
-            
-            if k in potassium_deficiency:
-                potassium_counter += 1
-            
-            if k in zinc_deficiency:
-                zinc_counter += 1
-            
-        results = {"Vitamin A": v_a_counter, "Vitamin B6": v_b_6_counter, "Vitamin B12": v_b_12_counter, "Vitamin C": v_c_counter, 
-        "Vitamin D": v_d_counter, "Vitamin E": v_e_counter, "Calcium": calcium_counter, 
-        "Iron": iron_counter, "Magnesium": magnesium_counter, "Potassium": potassium_counter, "Zinc": zinc_counter}
-        
-        
-        
-        
-        return keywords
-        
 
     def get_status(self):
         
@@ -216,7 +162,7 @@ def main():
 
     pregnancy_status = input("Are you pregnant (Y or N)? ")
     pregnancy_status = pregnancy_status.upper()
-    
+
     # Checks if pregnancy_status is a valid input
     while pregnancy_status != "Y" and pregnancy_status != "N":
         print("Invalid Entry.")
@@ -232,11 +178,7 @@ def main():
         breastfeeding_status = input("Are you pregnant (Y or N)? ")
         breastfeeding_status = breastfeeding_status.upper()
 
-    print("\nDo you think you could be vitamin or mineral deficient?")
-    user_symptoms = input("In one paragraph, please describe any symptoms you are feeling or noticing (i.e. fatigue, unsteady walking or movement, etc.): ")
-    
-    user = User(user_name, user_age, user_height, user_weight, pregnancy_status, breastfeeding_status, user_symptoms)
-    print(user.analyze_symptoms())
+    user = User(user_name, user_age, user_height, user_weight, pregnancy_status, breastfeeding_status)
     print(user.get_status())
 
 main()
